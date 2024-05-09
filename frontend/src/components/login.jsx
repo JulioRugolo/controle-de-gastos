a88cf33a82fab5a8aec5710b1d835ce6aac9f213
julio.rugolo <juliorugolo@gmail.com>
2024-05-09 14:00:03 -0300

    fix cors and url

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -10,7 +10,7 @@ function LoginForm() {
   const handleLogin = async (event) => {
     event.preventDefault();
     try {
-      const response = await axios.post('mdiniz-studio.railway.internal/api/users/login', {
+      const response = await axios.post('https://mdiniz-studio-production-3796.up.railway.app/api/users/login', {
         username,
         password
       });
