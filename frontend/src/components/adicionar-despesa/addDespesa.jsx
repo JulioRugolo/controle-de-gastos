35eada430258b634dd4a3eed271f99907910a7fb
julio.rugolo <juliorugolo@gmail.com>
2024-05-15 13:43:00 -0300

    fix: file type

--- frontend/src/components/adicionar-despesa/addDespesa.jsx
+++ frontend/src/components/adicionar-despesa/addDespesa.jsx
@@ -102,6 +102,7 @@ const AdicionarDespesaForm = () => {
             type="file"
             id="comprovante"
             name="comprovante"
+            accept='image/*,application/pdf'
             onChange={handleFileChange}
             className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
           />
