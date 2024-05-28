b90c377a38faa3d5b41a801eb992b07504a9a18b
julio.rugolo <juliorugolo@gmail.com>
2024-05-28 09:19:07 -0300

    update download link

--- frontend/src/components/download/desktop.jsx
+++ frontend/src/components/download/desktop.jsx
@@ -3,7 +3,9 @@ const DesktopDownload = () => {
     <div className="download-container">
       <h2>Download para Desktop</h2>
       <p>Clique no botão abaixo para baixar o instalador do nosso aplicativo para Windows.</p>
-      <a href="/path/to/your/application.exe" className="download-button">Baixar Aplicativo</a>
+      <a href="/downloads/Controle-de-Gastos.exe" className="download-button">Baixar Aplicativo</a>
+
+
     </div>
   );
 };
